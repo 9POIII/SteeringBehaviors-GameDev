e42c5dc41793982fc391208250cb26ab77f46170
Mykytenko Vsevolod <vsevolod200229@gmail.com>
2022-06-16 22:24:16 +0300

    Adding animal beehaviours

---
+++ Assets/Scripts/Beehaviours/Animal.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using MyBox;
+using Project.Scripts;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+        [SerializeField] private Rigidbody2D rigidbody2D;
+
+        private Vector2 velocity;
+        private Vector2 acceleration;
+        
+        [SerializeField] private float mass = 1;
+        [SerializeField, Range(1, 20)] private float velocityLimit = 3;
+        [SerializeField, Range(1, 50)] private float steeringForceLimit = 5;           
+
+        private const float Epsilon = 0.05f;
+        public float VelocityLimit => velocityLimit;
+        public Vector2 Velocity => velocity;
+
+        public void ApplyForce(Vector2 force)
+        {
+            force /= mass;
+            acceleration += force;
+        }
+
+        protected void Update()
+        {
+            ApplyFriction();
+            ApplySteeringForce();
+            ApplyForces();
+
+            void ApplyFriction()
+            {
+                var friction = -velocity.normalized * 0.5f;
+                ApplyForce(friction);
+            }
+
+            void ApplySteeringForce()
+            {
+                var providers = GetComponents<DesiredVelocityProvider>();
+                var steering = Vector2.zero;
+                foreach (var provider in providers)
+                {
+                    var desiredVelocity = provider.GetDesiredVelocity() * provider.Weight; //
+                    steering += desiredVelocity - velocity;
+                        
+                }
+
+                rigidbody2D.velocity = Vector2.ClampMagnitude(rigidbody2D.velocity + Vector2.ClampMagnitude(steering, steeringForceLimit), VelocityLimit);
+            }
+
+            void ApplyForces()
+            {
+                velocity += acceleration * Time.deltaTime;
+                velocity = Vector2.ClampMagnitude(velocity, velocityLimit);
+                if (velocity.magnitude < Epsilon)
+                {
+                    velocity = Vector2.zero;
+                    return;
+                }
+                
+                var transformPosition = (Vector2)transform.position;
+                transformPosition += velocity * Time.deltaTime;
+                //transform.position += velocity * Time.deltaTime;
+                acceleration = Vector2.zero;
+                //transform.rotation = Quaternion.LookRotation(velocity);
+            }
+        }
+}
---
+++ Assets/Scripts/Beehaviours/AvoidExitFromMap.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using MyBox;
+using UnityEngine;
+
+public class AvoidExitFromMap : DesiredVelocityProvider
+{
+    [SerializeField] private Transform topLeftDot;
+    [SerializeField] private Transform bottomRightDot;
+
+    private float edge = 0.05f;
+
+    public void Start()
+    {
+        topLeftDot = GameObject.FindGameObjectWithTag("TLD").transform;
+        bottomRightDot = GameObject.FindGameObjectWithTag("BRD").transform;
+    }
+
+    public Vector2 GetPositionInRelativeFormat(Vector2 position) {
+        var topLeftPos = topLeftDot.position;
+        var bottomRightPos = bottomRightDot.position;
+        var relativeX = (position.x - topLeftPos.x) / (bottomRightPos.x - topLeftPos.x);
+        var relativeY= (position.y - bottomRightPos.y) / (topLeftPos.y - bottomRightPos.y);
+        return new Vector2(relativeX, relativeY);
+    }
+    public override Vector2 GetDesiredVelocity()
+    {
+        var point = GetPositionInRelativeFormat(transform.position.ToVector2());
+        var maxSpeed = Animal.VelocityLimit;
+        var result = Vector2.zero;
+
+        if (point.x > 1 - edge) {
+            result += new Vector2(-maxSpeed, 0);
+        }
+        if (point.x < edge) {
+            result += new Vector2(maxSpeed, 0);
+        }
+
+        if (point.y > 1 - edge) {
+            result += new Vector2(0, -maxSpeed);
+        }
+        if (point.y < edge) {
+            result += new Vector2(0, maxSpeed);
+        }
+
+        return result.magnitude == 0 ? Animal.Velocity : result.normalized * maxSpeed;
+    }
+}
